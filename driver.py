f419e74397829210adfbf58b4582cb79926a2d97
Phrygan <honga010807@gmail.com>
2024-11-16 16:55:40 -0500

    listen on a port

--- driver.py
+++ driver.py
@@ -8,6 +8,21 @@ import manage_db as db
 import profile_commands
 import reserv_commands
 
+# listen on a port for render lmao
+import socket
+
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to an address and port
+server_address = ('localhost', 8080)
+server_socket.bind(server_address)
+
+# Listen for incoming connections
+server_socket.listen(1)
+
+print('Server listening on {}:{}'.format(*server_address))
+
 def get_token():
     dotenv.load_dotenv()
     return os.getenv("BOT_TOKEN")
